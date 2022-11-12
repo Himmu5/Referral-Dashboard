5c8f6e0724a20c99e558901113dcae28f044875a
Himanshu Chauhan <90271958+Himmu5@users.noreply.github.com>
2022-11-12 13:55:45 +0530

    Faltu fix

--- src/Refer&Earn/Process.jsx
+++ src/Refer&Earn/Process.jsx
@@ -2,10 +2,10 @@ function Process({path ,ExtraClass , heading ,para }) {
   return (
     <div>
       <div className="flex gap-8 items-center mt-4">
-        <div className={"bg-[#E5E5E5] rounded-full py-4 px-6 " + ExtraClass} >
-          <img src={path} className={"h-7"} alt="" />
+        <div className={"bg-[#E5E5E5] rounded-full py-5 px-6 " + ExtraClass} >
+          <img src={path} className={"h-7 " } alt="" />
         </div>
-        <div className="md:w-80 ">
+        <div className="w-80 ">
           <h1 className="font-bold"> {heading}</h1>
           <p>{para}</p>
         </div>
